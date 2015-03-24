141f1735a9c3df13dba8aa5001d9d60224ee3e80
Troy Connor <troy.connor@yahoo.com>
2015-03-23 20:45:27 -0400

    Added index.js back to normal.

--- routes/index.js
+++ routes/index.js
@@ -45,7 +45,7 @@ async.mapSeries(stuff, function (word, next) {
 
 });
 
-/*router.get('/:id', function(req, res) {
+router.get('/:id', function(req, res) {
 
 var phrase = 'What is the best language';
 var key = "TZlKw4gd1qFQhA2CbMLa";
@@ -79,52 +79,6 @@ function (error, results) {
 );
 
 });
-*/
-router.get('/:id', function(req, res) {
-'use strict';
 
-var bpromise = require('bluebird'),
-    request = require('request'),
-    _ = require('lodash');
-
-/* fn takes a phrase  - may the force be with you
-   which word has the most synonyms */
-
-var key = "TZlKw4gd1qFQhA2CbMLa";
-var urlBase = "http://thesaurus.altervista.org/service.php?language=en_US&output=json&key=" + key + "&word=";
-
-
-function syncount(phrase) {
-    var words = phrase.split(' ');
-
-    return bpromise.map(words, function (word) {
-        return new bpromise(function(resolve) {
-            request.get(urlBase + word, function (err, res, payload) {
-                payload = JSON.parse(payload);
-                if (payload.response) {
-                    var words = payload.response.reduce(function (words, item) {
-                        return words.concat(item.list.synonyms.split('|'));
-                    }, []);
-                    words = _.uniq(words);
-                    resolve(words.length);
-                } else {
-                    resolve(0);
-                }
-            }, {concurrecy: 1});
-        })
-
-    }).then(function (data) {
-        var max = _.max(data);
-        var index = _.findIndex(data, function (item) {
-            return item === max;
-        });
-
-        console.log(words[index], max);
-
-    })
-}
-
-syncount('may the force be with you');
-});
 
 module.exports = router;
